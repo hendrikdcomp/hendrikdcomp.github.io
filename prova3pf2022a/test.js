c8d4818127d1a61c8ba5526031cc3145c4e9df7c
Hendrik Macedo <hendrik@iMac-de-Hendrik.local>
2022-10-24 17:33:51 -0300

    simulado prova3 v2

--- prova3pf2022a/test.js
+++ prova3pf2022a/test.js
@@ -3,19 +3,6 @@ const FAIL = '\u274C'
 const UNABLE = '\u2753'
 const log = (msg) => console.log(msg)
 
-// const assert = (fstr,value) => {
-//     try {
-//         const f = fstr.subs
-//         if (f.apply(this,params) === value) return log(`${f.name} ${CHECK}`)
-//         else return log(`${f.name} ${FAIL}`)
-//     } catch(err) {
-//         log(`${f.name} ${UNABLE} ${err}`)
-//     }
-// }
-const equals = (list1, list2) =>
-  list1.length === list2.length &&
-  list1.every((value, i) => value === list2[i])
-
 /**
  * @param {any} result resultado da aplicação da função
  * @param {any} value Resultado correto
@@ -36,8 +23,9 @@ const assert = (result,value,f='') => {
  * em uma dada função. 
  */
 const checkRecursion = (f) => {
-    const expr = (`${f}`.match(RegExp(`${f.name}`,'gm'))||[]).length > +(/function/.test(f))
-    if (expr) return log(`${f.name} {recursion ${CHECK}}`)
+    const expr = (`${f}`.match(RegExp(`${f.name}`,'gm'))||[]).length > 0
+    const exprhelper = (`${f}`.match(RegExp('helper','gm'))||[]).length > 2
+    if (expr || exprhelper) return log(`${f.name} {recursion ${CHECK}}`)
     else return log(`${f.name} {recursion ${FAIL}}`)
 }
 
@@ -47,8 +35,8 @@ const checkRecursion = (f) => {
   */
 const checkOp = (f) => (op) => {
     const expr = !`${f}`.match(RegExp(op, 'gm'))
-    if (expr) return log(`${f.name} {avoid op ${CHECK}}`)
-    else return log(`${f.name} {avoid op ${FAIL}}`)
+    if (expr) return log(`${f.name} {avoid ${op} ${CHECK}}`)
+    else return log(`${f.name} {avoid ${op} ${FAIL}}`)
 }
 
 module.exports = {assert,checkRecursion,checkOp}
